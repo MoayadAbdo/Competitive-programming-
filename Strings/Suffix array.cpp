f6bd3bfc709a01024c68fb59ef33d7736b0973cd
Moayad Abdo <150342328+MoayadAbdo@users.noreply.github.com>
2025-08-28 22:37:26 +0300

    Create Suffix array.cpp

---
+++ Strings/Suffix array.cpp
@@ -0,0 +1,103 @@
+void count_sort(vector<int> &p, vector<int> &c)
+{
+    int n = p.size();
+    vector<int> cnt(n);
+    for (auto x : c)
+    {
+        cnt[x]++;
+    }
+    vector<int> p_new(n);
+    vector<int> pos(n);
+    pos[0] = 0;
+    for (int i = 1; i < n; i++)
+    {
+        pos[i] = pos[i - 1] + cnt[i - 1];
+    }
+    for (auto x : p)
+    {
+        int i = c[x];
+        p_new[pos[i]] = x;
+        pos[i]++;
+    }
+    p = p_new;
+}
+void solve()
+{
+    // O(NlogN)
+    string s;
+    cin >> s;
+    s += '$';
+    int n = s.size();
+    vector<int> p(n), c(n);
+    {
+        // k = 0
+        vector<pair<char, int>> a(n);
+        for (int i = 0; i < n; i++)
+            a[i] = {s[i], i};
+        sort(all(a));
+        for (int i = 0; i < n; i++)
+            p[i] = a[i].ss;
+        c[p[0]] = 0;
+        for (int i = 1; i < n; i++)
+        {
+            if (a[i].ff == a[i - 1].ff)
+            {
+                c[p[i]] = c[p[i - 1]];
+            }
+            else
+            {
+                c[p[i]] = c[p[i - 1]] + 1;
+            }
+        }
+    }
+    int k = 0;
+    while ((1 << k) < n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            p[i] = (p[i] - (1 << k) + n) % n;
+        }
+        count_sort(p, c);
+        vector<int> c_new(n);
+        c_new[p[0]] = 0;
+        for (int i = 1; i < n; i++)
+        {
+            pair<int, int> prev = {c[p[i - 1]], c[(p[i - 1] + (1 << k)) % n]};
+            pair<int, int> now = {c[p[i]], c[(p[i] + (1 << k)) % n]};
+            if (now == prev)
+            {
+                c_new[p[i]] = c_new[p[i - 1]];
+            }
+            else
+            {
+
+                c_new[p[i]] = c_new[p[i - 1]] + 1;
+            }
+        }
+        c = c_new;
+        k++;
+    }
+    //O(N) per query 
+    vector<int> lcp(n);
+    k = 0; // number of equals
+    for (int i = 0; i < n - 1; i++)
+    {
+        int pi = c[i]; // pos of suffix i
+        int j = p[pi - 1];
+        // lcp[i]=lcp(s[i..],s[j..])
+        while (s[i + k] == s[j + k])
+            k++;
+        lcp[pi] = k;
+        k = max(k - 1, 0);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << p[i] << " ";
+    }
+    cout << endl;
+
+    for (int i = 1; i < n; i++)
+    {
+        cout << lcp[i] << " ";
+    }
+}
