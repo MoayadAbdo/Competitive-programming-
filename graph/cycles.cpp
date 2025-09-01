dfda8d1a276b973eb4ac526a91f3a56d99c0d89e
Moayad Abdo <150342328+MoayadAbdo@users.noreply.github.com>
2025-09-01 16:12:28 +0300

    Update cycles.cpp

--- graph/cycles.cpp
+++ graph/cycles.cpp
@@ -97,3 +97,125 @@ void find_cycle() {
         cout << endl;
     }
 }
+//Bellman-Ford algorithm for a cycle with value (if asked about positive values *-1 to weights)
+struct Edge
+{
+    int u, v;
+    ll w;
+};
+
+int n, m;
+vector<Edge> edges;
+
+void solve()
+{
+    cin >> n >> m;
+    edges.resize(m);
+    for (int i = 0; i < m; i++)
+    {
+        cin >> edges[i].u >> edges[i].v >> edges[i].w;
+    }
+    vector<ll> dist(n + 1, 0);
+    vector<int> parent(n + 1, -1);
+    int x = -1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        x = -1;
+        for (auto &e : edges)
+        {
+            if (dist[e.v] > dist[e.u] + e.w)
+            {
+                dist[e.v] = max(-INF, dist[e.u] + e.w);
+                parent[e.v] = e.u;
+                x = e.v;
+            }
+        }
+    }
+    if (x == -1)
+    {
+        cout << "NO" << endl;
+    }
+    else
+    {
+        cout << "YES" << endl;
+        int y = x;
+        for (int i = 0; i < n; i++)
+            y = parent[y];
+
+        vector<int> cycle;
+        for (int cur = y;; cur = parent[cur])
+        {
+            cycle.push_back(cur);
+            if (cur == y && cycle.size() > 1)
+                break;
+        }
+        reverse(cycle.begin(), cycle.end());
+        for (int v : cycle)
+            cout << v << " ";
+        cout << endl;
+    }
+}
+//to find a cycle of length k+1 <= len 
+const int N = 1e5 + 5;
+vector<int> adj[N];
+int n, m, k;
+vector<int> parent(N, -1), depth(N, 0);
+vector<bool> visited(N, false);
+
+void printt(int u, int v)
+{
+    vector<int> cycle;
+    while (u != v)
+    {
+        cycle.push_back(u);
+        u = parent[u];
+    }
+    cycle.push_back(v);
+    reverse(cycle.begin(), cycle.end());
+
+    cout << cycle.size() << "\n";
+    for (int x : cycle)
+        cout << x << " ";
+    cout << "\n";
+    exit(0);
+}
+
+void dfs(int v)
+{
+    visited[v] = true;
+    for (int u : adj[v])
+    {
+        if (u == parent[v])
+            continue;
+        if (!visited[u])
+        {
+            parent[u] = v;
+            depth[u] = depth[v] + 1;
+            dfs(u);
+        }
+        else if (depth[v] - depth[u] + 1 >= k + 1)
+        {
+
+            printt(v, u);
+        }
+    }
+}
+void solve(){
+    cin>>n>>m>>k;
+    while(m--){
+        int x,y;cin>>x>>y;
+        adj[x].pb(y);
+        adj[y].pb(x);
+    }
+    for(int i=1;i<=n;i++){
+        if(!visited[i])dfs(i);
+    }
+}
+/*
+Positive cycle detection → negate weights + Bellman–Ford.
+
+Maximum-weight simple cycle → NP-hard (only small graphs possible).
+
+Maximum mean-weight cycle → use Karp’s algorithm.
+*/
