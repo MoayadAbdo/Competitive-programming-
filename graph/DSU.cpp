a9686011885bbcefcf887abd29646e6c2f401a5d
Moayad Abdo <150342328+MoayadAbdo@users.noreply.github.com>
2025-09-14 18:18:02 +0300

    Update DSU.cpp

--- graph/DSU.cpp
+++ graph/DSU.cpp
@@ -1,6 +1,11 @@
 /*
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+
 ordered_set s;
 s.insert(8);
 *(s.find_by_order(0)
