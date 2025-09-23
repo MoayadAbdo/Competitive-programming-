5c57b40d1f8c11786c78b2693c24385e94e2a22d
Moayad Abdo <150342328+MoayadAbdo@users.noreply.github.com>
2025-09-23 22:29:01 +0300

    Update ordered set.cpp

--- graph/ordered set.cpp
+++ graph/ordered set.cpp
@@ -65,3 +65,84 @@ s.order_of_key(8)
 if (s.find(2) != s.end()) 
     s.erase(s.find(2)); 
 */
+
+
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+class OrderedSet {
+private:
+    typedef tree<long long,
+            null_type,
+            less_equal<long long>,
+            rb_tree_tag,
+            tree_order_statistics_node_update> ordered_set;
+    ordered_set s;
+ 
+public:
+    void Insert(long long x) {
+        s.insert(x);
+    }
+ 
+    bool Exist(long long x) {
+        if (s.upper_bound(x) == s.end()) {
+            return false;
+        }
+        return (*s.upper_bound(x)) == x;
+    }
+ 
+    void Erase(long long x) {
+        if (Exist(x)) {
+            s.erase(s.upper_bound(x));
+        }
+    }
+ 
+    int Idx(long long x) {
+        if (!Exist(x)) {
+            return -1;
+        }
+        return s.order_of_key(x);
+    }
+ 
+    int LastIdx(long long x) {
+        if (!Exist(x)) {
+            return -1;
+        }
+        if (*s.find_by_order(s.size() - 1) == x) {
+            return s.size() - 1;
+        }
+        return Idx(*s.lower_bound(x)) - 1;
+    }
+ 
+    long long Value(int index) {
+        return *s.find_by_order(index);
+    }
+ 
+    int Count(long long x) {
+        if (!Exist(x)) {
+            return 0;
+        }
+        return LastIdx(x) - Idx(x) + 1;
+    }
+ 
+    void Clear() {
+        s.clear();
+    }
+ 
+    int Size() {
+        return s.size();
+    }
+ 
+    int Lower_bound(int x)
+    {
+ 
+            return *s.upper_bound(x);
+    }
+ 
+    int Upper_bound(int x)
+    {
+ 
+            return *s.lower_bound(x);
+    }
+};
